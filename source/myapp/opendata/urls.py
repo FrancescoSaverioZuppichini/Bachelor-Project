8b77889b320853690f4abe449b8c8fd46340539b
VaeVictis <VaeVictis@vaevictiss-mbp.home>
2017-02-25 18:06:21 +0100

    stable version of basic open data api

--- source/myapp/opendata/urls.py
+++ source/myapp/opendata/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^/locations',views.opendata_api_location, name="opendata_api_location"),
     url(r'^/connections', views.opendata_api_connections, name="opendata_api_connections"),
-    url(r'^/stationboard', views.opendata_api_opendata_api_stationboard, name="opendata_api_opendata_api_stationboard"),
+    url(r'^/stationboard', views.opendata_api_stationboard, name="opendata_api_stationboard"),
 
 ]
