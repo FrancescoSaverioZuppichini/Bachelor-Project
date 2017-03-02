5e2746192fc79cdd5628e07311123f734efe595b
VaeVictis <VaeVictis@vaevictiss-mbp.home>
2017-03-02 14:52:42 +0100

    ignore

--- source/myapp/opendata/opendataDisplay/src/components/transportCard/transportCard.vue
+++
@@ -1,4 +0,0 @@
-<template><div id='transportCard'></div></template>
-<script> import transportCardInfo from './transportCardInfo/transportCardInfo.vue'
- export default { name: 'transportCard', components: { transportCardInfo, }, } </script>
-<style></style>
