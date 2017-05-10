880ed6cf0a4079b12733d6d34fa4d5b01248c5d3
VaeVictis <VaeVictis@vaevictiss-mbp.home>
2017-05-10 21:21:53 +0200

    added proxy

--- utils/socket_client_server.py
+++ utils/socket_client_server.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
+from time import sleep
 
 from websocket import create_connection
 import json
-HOST = "ws://0.0.0.0:8080/ws"
+HOST = "ws://0.0.0.0:8003/ws"
 
 ws = create_connection(HOST)
 
 try:
   while True:
-    print(ws.recv())
     input("Trigger nearby user")
-    ws.send(json.dumps({"type": "USER_NEARBY", "payload": {"userId": 1}}))
+    ws.send(json.dumps({"type": "USER_NEARBY", "payload": {"userId": 1, "displayId":1} }))
+    print(ws.recv())
+
 
 except KeyboardInterrupt:
   print("")
