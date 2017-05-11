4e9d9af822505590fe08bc51db0d46ea675a83ac
VaeVictis <VaeVictis@vaevictiss-mbp.home>
2017-05-11 09:50:00 +0200

    added course

---
+++ source/transportWebServer/coursesWebApp/src/components/Course/Course.vue
@@ -0,0 +1,24 @@
+<template>
+<div class='uk-card uk-card-default uk-card-body'>
+  <div class='uk-card-title'>
+    {{course.name_en}}
+  </div>
+  <h3 class='uk-margin-remove'>{{course.professor_full_name}}</h3>
+  <p v-html="course.description_it">
+    <!-- {{course.description_it}} -->
+  </p>
+</div>
+</template>
+<script>
+export default {
+  name: "",
+  props: ['course'],
+  data: function data() {
+    return {
+
+    }
+  }
+}
+</script>
+<style>
+</style>
