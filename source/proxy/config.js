92956347ed344b375761d6a904e8af65ec36e512
VaeVictis <VaeVictis@vaevictiss-mbp.home>
2017-05-11 12:48:44 +0200

    added application to proxy

--- source/proxy/config.js
+++ source/proxy/config.js
@@ -1,9 +1,9 @@
 const BASE_URL = 'http://localhost:'
 
-module.exports =  {
+module.exports = {
   'transport': BASE_URL + '8080',
   'auth': BASE_URL + '8081',
   'classes': BASE_URL + '8082',
   'user+display': BASE_URL + '8083',
-'application': BASE_URL + '8085'
+  'application': BASE_URL + '8085'
 }
