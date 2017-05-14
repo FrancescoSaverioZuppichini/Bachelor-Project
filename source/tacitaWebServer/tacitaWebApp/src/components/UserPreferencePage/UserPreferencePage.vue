48aa16fff0401fa9cdf5d32c1344777a29f59412
VaeVictis <VaeVictis@vaevictiss-mbp.home>
2017-05-14 20:21:03 +0200

    minor changes

---
+++ source/tacitaWebServer/tacitaWebApp/src/components/UserPreferencePage/UserPreferencePage.vue
@@ -0,0 +1,55 @@
+<template>
+<div class='f-h'>
+  <choice-color :colors='colors' radius='10em' @updateColor='updateColor'></choice-color>
+</div>
+</template>
+<script>
+import Vue from 'vue'
+
+import {
+  choiceColor
+} from 'vue-circle-choice'
+// import {materialColors} from './colors.js'
+
+const colors = [
+  '#F44336',
+  '#E91E63',
+  '#9C27B0',
+  '#673AB7',
+  '#3F51B5',
+  '#2196F3',
+  '#03A9F4',
+  '#00BCD4',
+  '#009688',
+  '#FFEB3B',
+  '#FF9800',
+  '#795548',
+  '#607D8B',
+];
+
+
+export default {
+  name: "",
+  components: {
+    choiceColor
+  },
+  data() {
+    return {
+      colors,
+      index: 0,
+      color: colors[0],
+    }
+  },
+  methods: {
+    updateColor({
+      index,
+      color
+    }) {
+      this.index = index;
+      this.color = color;
+    }
+  }
+}
+</script>
+<style>
+</style>
