890a7e4fb58a6706a1f2b572d4db299037c352b0
VaeVictis <VaeVictis@vaevictiss-mbp.home>
2017-05-18 20:50:04 +0200

    added some components

---
+++ source/tacitaWebServer/tacitaWebApp/src/components/UserPreferencePage/ColorPickerPage/ColorPickerPage.vue
@@ -0,0 +1,66 @@
+<template>
+<page>
+  <div id="ColorPicker" class='uk-flex-column uk-flex-center uk-margin-top'>
+    <h4 class='uk-text-center'>Pick a new color</h4>
+    <choice-color :colors='colors' radius='10em' @updateColor='updateColor'></choice-color>
+    <div class="bottom-container--absolute uk-flex uk-flex-center" :style="{'bottom':'60px'}">
+      <v-btn primary light @click.native="done()">Done</v-btn>
+    </div>
+  </div>
+</page>
+</template>
+<script>
+import Page from '../../Page.vue'
+import {
+  choiceColor
+} from 'vue-circle-choice'
+
+const colors = [
+  '#F44336',
+  '#E91E63',
+  '#9C27B0',
+  '#673AB7',
+  '#3F51B5',
+  '#2196F3',
+  '#03A9F4',
+  '#00BCD4',
+  '#009688',
+  '#FFEB3B',
+  '#FF9800',
+  '#795548',
+  '#607D8B',
+];
+
+export default {
+  name: "",
+  components: {
+    choiceColor,
+    Page
+  },
+  data() {
+    return {
+      colors,
+      index: 0,
+      color: colors[0],
+    }
+  },
+  methods: {
+    done() {
+      this.$store.actions.updateUser(this.$store.state.user)
+      this.$router.go(-1)
+    },
+    updateColor({
+      index,
+      color
+    }) {
+      this.index = index;
+      this.color = color;
+      this.$store.state.user.color = color
+    }
+  }
+
+}
+</script>
+<style scoped>
+
+</style>
---
+++ source/tacitaWebServer/tacitaWebApp/src/components/UserPreferencePage/PreferenceDummy.vue
@@ -0,0 +1,47 @@
+<template>
+<transition name="fade">
+  <div class='uk-card uk-card-body uk-card-default my-card__body uk-flex'>
+    <div class='user-color uk-margin-small-right' :style="{ 'background-color': $store.state.user.color, 'opacity':'0.5' }" v-if="$store.state.user.color"></div>
+    <div class='uk-flex-wrap-stretch uk-width-expand'>
+      <div class="uk-flex uk-flex-middle ">
+        <div class="dummy-preference__text dummy-preference--background-color" :style="{'width':'50%'}">
+        </div>
+      </div>
+      <div>
+        <div class='uk-flex uk-flex-column uk-flex-middle uk-margin-small-top'>
+          <div class="uk-margin-small-bottom uk-flex uk-flex-middle" v-for="index in [1]" :style="{'width':'100%'}">
+            <div class="dummy-preference__bus dummy-preference--background-color uk-margin-small-right"></div>
+            <div class="dummy-preference__text--small dummy-preference--background-color" :style="{'width':'70%'}"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</transition>
+</template>
+<script>
+export default {
+  name: "UserPreferenceDummy"
+}
+</script>
+<style scoped>
+.dummy-preference__text {
+  height: 16px;
+  width: 100%;
+}
+
+.dummy-preference__text--small {
+  height: 14px;
+  width: 100%;
+}
+
+.dummy-preference__bus {
+  width: 24px;
+  height: 24px;
+}
+
+.dummy-preference--background-color {
+  background-color: rgb(246, 246, 246) !important;
+}
+</style>
