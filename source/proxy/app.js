05ef4441b63584c93df29c08f0a0f65c88ba09d6
VaeVictis <VaeVictis@vaevictiss-mbp.mobile.usilu.net>
2017-05-26 11:35:47 +0200

    minor changes

--- source/proxy/app.js
+++ source/proxy/app.js
@@ -2,31 +2,34 @@ var express = require('express');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-const fs = require('fs')
-const https = require('https');
+var fs = require('fs')
+
+var https = require('https');
 var http = require('http');
 
+var proxy = require('express-http-proxy');
+
+
+const url = require('url')
+const axios = require('axios')
+const config = require('./config.js');
+
+var app = express();
+
+app.use(logger('dev'));
+
+app.use(cookieParser());
+
 var options = {}
+
 try {
   options = {
     cert: fs.readFileSync('./sslcert/fullchain.pem'),
     key: fs.readFileSync('./sslcert/privkey.pem')
   }
 } catch (err) {
-
+  console.error(err)
 }
-
-var proxy = require('express-http-proxy');
-
-var app = express();
-
-const url = require('url')
-const axios = require('axios')
-const config = require('./config.js');
-
-app.use(logger('dev'));
-
-app.use(cookieParser());
 
 // handle the js case
 app.use('/js/*', (req, res) => {
@@ -45,12 +48,11 @@ for (let key in config) {
   }))
 }
 
-console.log(options)
+var port = process.env.PORT || '3000';
 
-var port =process.env.PORT || '3000' ;
 app.set('port', 3000);
 
-var server = http.createServer(app).listen(port);
+var server = http.createServer(app).listen(3000);
 https.createServer(options, app).listen(3443);
 
 module.exports = app;
