a738c5bbe24b47dc19f1ac812cd3197e59bfab5c
VaeVictis <VaeVictis@vaevictiss-mbp.mobile.usilu.net>
2017-06-26 21:40:13 +0200

    fixed offset transport

--- source/transportWebServer/transportWebApp/src/components/CreatePreferencePage/Resource/ResourceTransitionWrapper.vue
+++ source/transportWebServer/transportWebApp/src/components/CreatePreferencePage/Resource/ResourceTransitionWrapper.vue
@@ -1,7 +1,7 @@
 <template>
 <!-- <transition-group name="custom-classes-transition" enter-active-class="animated fadeInUp"  class='uk-flex uk-flex-column uk-grid-small' uk-grid> -->
 
-<transition-group name="custom-classes-transition" class='uk-flex uk-flex-column uk-grid-small' uk-grid>
+<transition-group name="custom-classes-transition" class='uk-flex uk-flex-column uk-grid-small container--offset' uk-grid>
   <slot></slot>
 </transition-group>
 </template>
